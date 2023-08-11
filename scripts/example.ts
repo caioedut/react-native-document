cf458ae5bd9d06a81ae0da5037368963ed3b6b50
Caio Teixeira <caio.teixeira@unyleya.com.br>
2023-08-11 16:53:12 -0300

    chore: scripts

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,17 +1,19 @@
 import pmex from 'pmex';
 
-import { rmSync } from 'fs';
+import { rmSync, cpSync } from 'fs';
+
+const { name } = require('../package.json');
 
 const cwd = './example';
 
-pmex('tsc --build --force');
-
-rmSync(`${cwd}/yarn.lock`, { force: true });
-
-rmSync(`${cwd}/package-lock.json`, { force: true });
-
-rmSync(`${cwd}/node_modules/react-native-document`, { force: true, recursive: true });
+pmex('build');
 
 pmex(`--cwd ${cwd} install`);
+
+rmSync(`${cwd}/node_modules/${name}/dist`, { force: true, recursive: true });
+
+cpSync('dist', `${cwd}/node_modules/${name}/dist`, { recursive: true, force: true });
+
+cpSync('package.json', `${cwd}/node_modules/${name}/package.json`, { force: true });
 
 pmex(`--cwd ${cwd} expo start -c`);
